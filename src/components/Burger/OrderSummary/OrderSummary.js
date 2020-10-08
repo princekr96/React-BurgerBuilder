ad181c7d85185a5a8a8f9edd2b6dc51ce212244a
unknown <princekr700@gmail.com>
2020-10-08 14:49:05 +0530

    commit

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>);
+        });
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A Delicious Burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: {props.price}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </Aux>
+    )
+};
+
+export default orderSummary;
