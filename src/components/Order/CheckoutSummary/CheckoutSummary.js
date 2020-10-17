990198549cb25843903891ee5b579cf66c17fbe0
unknown <princekr700@gmail.com>
2020-10-17 16:20:12 +0530

    Latest

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -9,7 +9,7 @@ const checkoutSummary = (props) => {
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
             <div style={{ width: '100%', height: '300px', margin: 'auto' }}>
-                <Burger ingredients={props.ingredients} />
+                <Burger ingredients={props.ingredients} price={props.price} />
             </div>
             <Button
                 btnType="Danger"
